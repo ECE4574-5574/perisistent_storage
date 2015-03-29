803c7678a15f3211966593bb77042718c4290d26
Ubuntu <ubuntu@ip-172-31-18-216.ec2.internal>
2015-03-29 20:23:56 +0000

    Inserted basic house/room/device structures and mysql insertion.

---
+++ hats_persistent_storage/structures.py
@@ -0,0 +1,27 @@
+class Device:
+  def __init__(self, device_id, device_type, data):
+    self._device_id = device_id
+    self._device_type = device_type
+    self._data = data
+
+class Room:
+  def __init__(self, room_id, data, devices):
+    self._room_id = room_id
+    self._data = data
+
+    self._devices = []
+    for device in devices:
+      self._devices.append(device)
+
+class House:
+  def __init__(self, house_id, data, rooms, devices):
+    self._house_id = house_id
+    self._data = data
+
+    self._rooms = []
+    for room in rooms:
+      self._rooms.append(room)
+
+    self._devices = []
+    for device in devices:
+      self._devices.append(device)
