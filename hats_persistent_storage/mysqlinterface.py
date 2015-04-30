581ad01f5e6e5066b168559a5b743ae2288d0256
Graham Cantor-Cooke <grahamcc@vt.edu>
2015-04-30 13:49:10 -0400

    doing it correctly this time

--- hats_persistent_storage/mysqlinterface.py
+++ hats_persistent_storage/mysqlinterface.py
@@ -252,7 +252,7 @@ class MySQLInterface:
     for a_id, time, h_id, r_id, d_id, data in self._cur.fetchall():
       action_list.append(UserAction(a_id, time, h_id, r_id, d_id, data))
 
-    print action_list._time
+    print action_list[0]._time
     return action_list
 
 
