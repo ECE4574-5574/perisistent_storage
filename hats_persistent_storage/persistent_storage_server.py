3f26a24ce1ec5686a4fbc566de62af8a6c060990
Graham Cantor-Cooke <grahamcc@vt.edu>
2015-04-30 13:38:54 -0400

    return string instead of object

--- hats_persistent_storage/persistent_storage_server.py
+++ hats_persistent_storage/persistent_storage_server.py
@@ -168,7 +168,7 @@ class HATSPersistentStorageRequestHandler(BaseHTTPRequestHandler):
                     self.send_response(200)
                     self.send_header('Content-Type', 'application/json')
                     self.end_headers()
-                    self.wfile.write(blob) 
+                    self.wfile.write(str(blob)) 
                 else:
                     self.stubResponseOK()
             else:
